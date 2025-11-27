5e058d6ea6e272134b17afa7dfe6c47d657b2c95
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-11-27 19:24:35 +0100

    Added download-pages. (6)

--- docs-pages/downloads/generate-downloads-page.py
+++ docs-pages/downloads/generate-downloads-page.py
@@ -23,7 +23,7 @@ EMPTY_ICON = "\u2205"
 
 # Resolve output directory: we are running from docs-pages/downloads
 PARENT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-GRAND_PARENT_DIR = os.path.abspath(PARENT_DIR, ".."))
+GRAND_PARENT_DIR = os.path.abspath(os.path.join(PARENT_DIR, ".."))
 DOCS_DIR = os.path.join(GRAND_PARENT_DIR, "docs")
 DOWNLOADS_OUTPUT_DIR = os.path.join(DOCS_DIR, "downloads")
 os.makedirs(DOWNLOADS_OUTPUT_DIR, exist_ok=True)
